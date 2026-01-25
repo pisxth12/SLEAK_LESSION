1e11730e128accc9b5b71ea4024fb25b1fc9e058
pisxth12 <pxsxth1214@gmail.com>
2026-01-25 13:17:39 +0700

    git

---
+++ src/components/ChildProps.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChildProps = ({name, name2}) => {
+  return (
+    <h1>Hello my name : <strong> {name} </strong>and My boy friend's name : <strong>{name2}</strong></h1>
+  )
+}
+
+export default ChildProps
